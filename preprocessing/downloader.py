834b89a3d0eafb849df6aa7f9f9f3373b7d528ad
breandan.considine <breandan.considine@gmail.com>
2019-11-21 20:04:56 -0500

    use python2 instead

--- preprocessing/downloader.py
+++ preprocessing/downloader.py
@@ -1,21 +1,23 @@
 import json
 import os
 
-import urllib.request
+import urllib2
 import gzip
 import io
 
 def download_docs():
-    if not os.path.exists('archives'):
-        os.makedirs('archives')
+    if not os.path.exists('archives1'):
+        os.makedirs('archives1')
     with open('index.json') as json_file:
         data = json.load(json_file)
         for p in data['docsets']:
             archive = data['docsets'][p]['archive']
             url = 'http://newyork.kapeli.com/feeds/zzz/user_contributed/build/' + p + '/' + archive
-            with urllib.request.urlopen(url) as response, open('archives/' + archive, 'wb') as out_file:
-                out_file.write(response.read())
+            sock = urllib2.urlopen(url)
+            with open('archives1/' + archive, 'wb') as out_file:
+                out_file.write(sock.read())
                 print('Downloaded: ' + archive)
+            sock.close()
 
 if __name__ == '__main__':
     download_docs()
