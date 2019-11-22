244e90a12f7a259493719bbecb0b2f08e413f98e
breandan.considine <breandan.considine@gmail.com>
2019-11-21 20:30:11 -0500

    fix directory

--- preprocessing/downloader.py
+++ preprocessing/downloader.py
@@ -6,15 +6,15 @@ import gzip
 import io
 
 def download_docs():
-    if not os.path.exists('archives1'):
-        os.makedirs('archives1')
+    if not os.path.exists('archives'):
+        os.makedirs('archives')
     with open('index.json') as json_file:
         data = json.load(json_file)
         for p in data['docsets']:
             archive = data['docsets'][p]['archive']
             url = 'http://newyork.kapeli.com/feeds/zzz/user_contributed/build/' + p + '/' + archive
             sock = urllib2.urlopen(url)
-            with open('archives1/' + archive, 'wb') as out_file:
+            with open('archives/' + archive, 'wb') as out_file:
                 out_file.write(sock.read())
                 print('Downloaded: ' + archive)
             sock.close()
