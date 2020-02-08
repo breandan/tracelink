e30d4f6bd1b7d8f7eefa9abe8cd9a6a0708ac291
breandan <bre@ndan.co>
2020-02-08 18:31:37 -0500

    update proposal and add preliminary results

--- preprocessing/build.gradle.kts
+++ preprocessing/build.gradle.kts
@@ -1,47 +1,46 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.60"
-    application
+  kotlin("jvm") version "1.3.60"
+  application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
-    maven(url = "https://dl.bintray.com/egor-bogomolov/astminer/")
+  mavenCentral()
+  maven(url = "https://dl.bintray.com/egor-bogomolov/astminer/")
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jsoup:jsoup:1.12.1")
-    implementation("me.xdrop:fuzzywuzzy:1.2.0")
-    implementation("com.google.guava:guava:28.1-jre")
-    implementation("org.apache.commons:commons-compress:1.19")
-    implementation("org.apache.commons:commons-vfs2:2.4.1")
-    implementation("org.apache.lucene:lucene-core:8.3.0")
-    implementation("org.apache.lucene:lucene-queryparser:8.3.0")
-    implementation("org.apache.lucene:lucene-analyzers-common:8.3.0")
-    implementation("io.github.vovak.astminer", "astminer", "0.5")
+  implementation(kotlin("stdlib-jdk8"))
+  implementation("org.jsoup:jsoup:1.12.1")
+  implementation("me.xdrop:fuzzywuzzy:1.2.0")
+  implementation("com.google.guava:guava:28.1-jre")
+  implementation("org.apache.commons:commons-compress:1.19")
+  implementation("org.apache.commons:commons-vfs2:2.4.1")
+  implementation("org.apache.lucene:lucene-core:8.3.0")
+  implementation("org.apache.lucene:lucene-queryparser:8.3.0")
+  implementation("org.apache.lucene:lucene-analyzers-common:8.3.0")
+  implementation("io.github.vovak.astminer", "astminer", "0.5")
 }
 
 tasks {
-    listOf("ParseDocs", "ParseLinks", "ParseQueries", "QueryDocs")
-        .forEach {
-            register(it, JavaExec::class) {
-                findProperty("retty")?.let { args = listOf("1") }
-                findProperty("rocess")?.let { args = listOf(it.toString())}
-                main = "${it}Kt"
-                classpath = sourceSets["main"].runtimeClasspath
-            }
-        }
-
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
+  listOf("ParseDocs", "ParseLinks", "ParseQueries", "QueryDocs").forEach {
+    register(it, JavaExec::class) {
+      findProperty("retty")?.let { args = listOf("1") }
+      findProperty("rocess")?.let { args = listOf(it.toString()) }
+      main = "${it}Kt"
+      classpath = sourceSets["main"].runtimeClasspath
     }
+  }
+
+  withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+  }
 }
 
 configure<ApplicationPluginConvention> {
-    mainClassName = "ParseLinksKt"
+  mainClassName = "ParseLinksKt"
 }
